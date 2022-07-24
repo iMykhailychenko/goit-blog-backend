2d8f909cef387a262737a9351fd614da779bcfdb
imykhailychenko <igor.c.m@ukr.net>
2022-07-24 18:52:52 +0200

    cd

--- scripts/run-dev.sh
+++ scripts/run-dev.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+docker-compose stop
+docker image prune -a -f
 docker-compose -f docker-compose.dev.yml --env-file .env.dev up --build --remove-orphans -d
--- scripts/run-prod.sh
+++ scripts/run-prod.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+docker-compose stop
+docker image prune -a -f
 docker-compose -f docker-compose.yml --env-file .env up --build --remove-orphans -d
