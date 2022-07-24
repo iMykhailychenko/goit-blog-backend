c6ca982fb3d081450882a05648d0b5e38fd746ea
imykhailychenko <igor.c.m@ukr.net>
2022-07-24 19:36:42 +0200

    update

--- migrations/0001-posts.sql
+++ migrations/0001-posts.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS posts (
     id SERIAL PRIMARY KEY,
     title VARCHAR(250) NOT NULL,
-    content VARCHAR(1000) NOT NULL,
+    content VARCHAR(5000) NOT NULL,
     image VARCHAR(250),
     preview_image VARCHAR(250),
     views INT DEFAULT 0 NOT NULL,
