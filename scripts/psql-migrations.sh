e653c25378e99a5729f26d87723b256eddb875a3
imykhailychenko <igor.c.m@ukr.net>
2022-07-24 18:51:24 +0200

    cd

--- scripts/psql-migrations.sh
+++ scripts/psql-migrations.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for file in app/migrations/*
+for file in ../migrations/*
 do
     PGPASSWORD="$POSTGRES_PASSWORD" psql "$POSTGRES_USER" -h "$POSTGRES_HOST" -d "$POSTGRES_DB" -f "$(pwd)/$file" -a
 done
